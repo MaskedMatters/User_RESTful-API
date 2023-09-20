3c6a9b52c65282e4ee051ab190c419fb7d00804b
MaskedMatters <Masked.Matters@gmail.com>
2023-09-20 19:52:22 -0400

    Finished RESTful API *SERVER ONLY*
    There is a database in a JSON file. We also have the ability to list all users, list a specific user, add a user, and delete a user. It's all new!!

---
+++ index.js
@@ -0,0 +1,63 @@
+// Libraries
+const express = require('express');    // Library that handles the server endpoints
+const fs = require('fs');              // Library that handles READ/WRITE to files
+
+// Config
+const app = express();                 // Library constant set to app
+app.use( express.json() );             // All GET requests get parsed with JSON
+
+const PORT = 3000;                     // Port that will host the web app
+
+// --------------------------------------------------------------------
+// USER INSTANTIATION **VERY IMPORTANT** (LITERALLY NOT JOKING) A joke
+// --------------------------------------------------------------------
+let users = require('./users.json'); // <------------
+
+// Server Endpoints
+app.get('/users', (req, res) => {
+    res.status(200).json(users);
+});
+
+app.get('/user/:id', (req, res) => {
+    let user = users.find(x => x.id.toString() === req.params.id);
+    res.status(200).json(user);
+});
+
+app.post('/add', (req, res) => {
+    let user = {
+        id : Math.max(...users.map(user => user.id)) + 1,
+        email : req.body.email,
+        username : req.body.username,
+        password : req.body.password
+    };
+
+    users.push(user);
+
+    fs.writeFile('./users.json', JSON.stringify(users, null, 2), function(err) {
+        if (err) {
+           res.status(500).send(err);
+        }
+    });
+
+    res.status(200).json(user);
+    res.send("Successfully done... I hope!!");
+});
+
+app.delete('/delete/:id', (req, res) => {
+    let index = users.findIndex(x => x.id.toString() === req.params.id);
+    users.splice(index, 1);
+
+    fs.writeFile('./users.json', JSON.stringify(users, null, 2), function(err) {
+        if (err) {
+           res.status(500).send(err);
+        }
+    });
+
+    res.send("Successfully done... I hope!!");
+});
+
+// Server Hosting Starts
+app.listen(
+    PORT,
+    () => console.log('I am alive!! I mean, the server is... *cough cough*') // A joke
+);
