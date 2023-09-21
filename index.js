bb509d3fb51bbfdc1c4e65d5ce55ff0fb9472169
MaskedMatters <Masked.Matters@gmail.com>
2023-09-20 20:35:05 -0400

    Change from ID requests to username or email
    You can grab an object by their username or email instead of ID now. ID is completely removed.

--- index.js
+++ index.js
@@ -14,18 +14,22 @@ const PORT = 3000;                     // Port that will host the web app
 let users = require('./users.json'); // <------------
 
 // Server Endpoints
-app.get('/users', (req, res) => {
+app.get('/lsit/all', (req, res) => {
     res.status(200).json(users);
 });
 
-app.get('/user/:id', (req, res) => {
-    let user = users.find(x => x.id.toString() === req.params.id);
+app.get('/list/username/:username', (req, res) => {
+    let user = users.find(x => x.username.toString() === req.params.username);
+    res.status(200).json(user);
+});
+
+app.get('/list/email/:email', (req, res) => {
+    let user = users.find(x => x.email.toString() === req.params.email);
     res.status(200).json(user);
 });
 
 app.post('/add', (req, res) => {
     let user = {
-        id : Math.max(...users.map(user => user.id)) + 1,
         email : req.body.email,
         username : req.body.username,
         password : req.body.password
@@ -43,8 +47,21 @@ app.post('/add', (req, res) => {
     res.send("Successfully done... I hope!!");
 });
 
-app.delete('/delete/:id', (req, res) => {
-    let index = users.findIndex(x => x.id.toString() === req.params.id);
+app.delete('/delete/username/:username', (req, res) => {
+    let index = users.findIndex(x => x.username.toString() === req.params.username);
+    users.splice(index, 1);
+
+    fs.writeFile('./users.json', JSON.stringify(users, null, 2), function(err) {
+        if (err) {
+           res.status(500).send(err);
+        }
+    });
+
+    res.send("Successfully done... I hope!!");
+});
+
+app.delete('/delete/email/:email', (req, res) => {
+    let index = users.findIndex(x => x.email.toString() === req.params.email);
     users.splice(index, 1);
 
     fs.writeFile('./users.json', JSON.stringify(users, null, 2), function(err) {
